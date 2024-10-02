b1485069e5db0b397c7829b919ffcd1fe75eefd7
RooK| <anton.smishko2014@gmail.com>
2024-10-02 12:39:27 +0300

    update
    add btn load more, use URLSearchParams , and add style

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { linkEl } from '../main';
 const URL_PIXABAY = 'https://pixabay.com/api/';
 const API_KEY = '46219346-243694830f7cca451bf7f7da0';
+const PER_PAGE = 15;
 // робимо запит , збрираю url string , якщо все ок то повертаю дані у вигляді [{}], якщо не ок то генерую помилку та
 // очищаю innerHTML в ul
 // export function fetchImages(query) {
@@ -20,11 +21,25 @@ const API_KEY = '46219346-243694830f7cca451bf7f7da0';
 // }
 
 export async function fetchImages(query) {
+  // використовуємо обєкт параметрів пошуку
+
+  const searchParams = new URLSearchParams({
+    q: query,
+    per_page: PER_PAGE,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+  });
   try {
     const response = await axios.get(
-      `${URL_PIXABAY}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true
-`
+      `${URL_PIXABAY}?key=${API_KEY}&${searchParams}}`
     );
+
+    //     const response = await axios.get(
+    //       `${URL_PIXABAY}?key=${API_KEY}&q=${query}&per_page=${PER_PAGE}&image_type=photo&orientation=horizontal&safesearch=true
+    // `
+    //     );
 
     return response.data;
   } catch (error) {
